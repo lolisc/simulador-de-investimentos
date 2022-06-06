d1acbc62971c14a2ada48dac17ff43ac7bc8ae9b
Lorena <lorenanevescarneiroc@gmail.com>
2022-06-06 00:24:08 -0300

    completion of the project

--- index.js
+++ index.js
@@ -3,15 +3,6 @@ function getElementValue(id) {
   return element.value;
 }
 
-function saveForm() {
-  var nome = getElementValue("nome");
-  console.log(nome);
-  var mensalidade = getElementValue("mensalidade");
-  console.log(mensalidade);
-  var tempo = getElementValue("tempo");
-  console.log(tempo);
-}
-
 function saveLocalStorage() {
   var nome = getElementValue("nome");
   localStorage.setItem("nome", nome);
@@ -20,15 +11,14 @@ function saveLocalStorage() {
   var tempo = getElementValue("tempo");
   localStorage.setItem("tempo", tempo);
 }
-
+// retorna o href (URL) da página atual
 function redirectNewPage() {
   window.location.href = "resultado.html";
 }
+
 function submit() {
   saveLocalStorage();
   redirectNewPage();
-  // Retorno "false" para parar a ação padrão do evento, ou seja, o clique no botão. Por algum motivo sem o "false" ele não redireciona para a outra página.
-  return false;
 }
 
 var element = document.getElementById("submit");
--- resultado.js
+++ resultado.js
@@ -1,32 +1,61 @@
-// var myHeaders = new Headers();
-// myHeaders.append("Content-Type", "application/json");
+// Passei o nome da chave para o método getItem() retornando o seu valor, depois abri o objeto return com os nomes da minha id.
+function getDataFromLocalStorage() {
+  var nome = localStorage.getItem("nome");
+  var mensalidade = localStorage.getItem("mensalidade");
+  var tempo = localStorage.getItem("tempo");
 
-// var myInit = {
-//   method: "GET",
-//   headers: myHeaders,
-//   method: "POST",
-//   body: '{"expr": "20 * (((1 + 0.00517) ^ 24 - 1) / 0.00517)"}',
-// };
-
-// var url = "http://api.mathjs.org/v4/";
-// const myRequest = new Request(url, myInit);
-
-// fetch(myRequest)
-//   .then((response) => {
-//     if (response.status === 200) {
-//       return response.json();
-//     } else {
-//       throw new Error("Ops! Houve um erro em nosso servidor.");
-//     }
-//   })
-//   .then((response) => {
-//     console.debug(response);
-//     // ...
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
-function nome(name) {
-  return Olá + name;
+  return {
+    nome: nome,
+    tempo: tempo,
+    mensalidade: mensalidade,
+  };
 }
+
+// Enviando dados para a api
+function sendDataToApi(
+  valorMensalidade,
+  taxaJuros,
+  tempoContribuicao,
+  callback
+) {
+  var params = {
+    expr: `${valorMensalidade} * (((1 + ${taxaJuros}) ^ ${tempoContribuicao} - 1) / ${taxaJuros})`,
+  };
+
+  var url = new URL("http://api.mathjs.org/v4");
+  url.search = new URLSearchParams(params).toString();
+
+  var request = new Request(url, {
+    method: "GET",
+  });
+
+  fetch(request).then(function (response) {
+    response.json().then(function (data) {
+      callback(data);
+    });
+  });
+}
+
+function handleApiResult(result) {
+  var data = getDataFromLocalStorage();
+  var nf = new Intl.NumberFormat("pt-BR", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  var message = `Olá ${data.nome}, juntando R$ ${nf.format(
+    data.mensalidade
+  )} todo mês, você terá R$ ${nf.format(result)} em ${data.tempo} anos.`;
+
+  var resultElement = document.querySelector("#result > h2");
+  resultElement.innerHTML = message;
+}
+
+function process() {
+  const taxaJuros = 0.517;
+
+  var data = getDataFromLocalStorage();
+  sendDataToApi(data.mensalidade, taxaJuros, data.tempo, handleApiResult);
+}
+
+process();
